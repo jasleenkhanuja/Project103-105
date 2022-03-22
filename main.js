bb19cbe65fd75bcd67f8dbd9b13cd27cbfe9fbb6
jasleenkhanuja <85404467+jasleenkhanuja@users.noreply.github.com>
2022-03-22 16:18:57 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ console.log('ml5 version:', ml5.version);
 
   // Initialize the Image Classifier method with MobileNet
 //classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JuyzdXH-P/',modelLoaded);
  
   // When the model is loaded
   function modelLoaded() {
